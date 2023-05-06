ecdce38456d843bec69103d5130948bd4fbce689
ethantechtorial <115048941+ethantechtorial@users.noreply.github.com>
2023-05-06 21:39:07 +0200

    Update install.sh

--- code/k8s/install.sh
+++ code/k8s/install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo installing k8s api and client...
 
-kubectl create ns cloudacademy
+kubectl create ns techtorialcademy
 kubectl apply -f ./api.yaml
 
 echo installing k8s api client pod...
 kubectl run api-client \
-  --namespace=cloudacademy \
-  --image=cloudacademydevops/api-generator \
+  --namespace=techtorialcademy \
+  --image=ethantechtorial/api-generator \
   --env="API_URL=http://api-service:5000" \
   --image-pull-policy IfNotPresent
 
 echo "waiting for api and client to start (30-60secs)..."
-kubectl wait --for=condition=available --timeout=300s deployment/api -n cloudacademy
-kubectl wait --for=condition=ready --timeout=300s pod/api-client -n cloudacademy
+kubectl wait --for=condition=available --timeout=300s deployment/api -n techtorialcademy
+kubectl wait --for=condition=ready --timeout=300s pod/api-client -n techtorialcademy
 
-kubectl get all -n cloudacademy
+kubectl get all -n techtorialcademy
 
 echo k8s api and client install finished!
